4b4d1b0b485566135dc8b2282c5086a989171701
Haoxin Li <54lihaoxin@gmail.com>
2018-07-01 16:40:58 -0700

    create the empty framework

---
+++ PokipediaFoundation/PokipediaFoundation.h
@@ -0,0 +1,17 @@
+//
+//  PokipediaFoundation.h
+//  PokipediaFoundation
+//
+//  Created by Haoxin Li on 7/1/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PokipediaFoundation.
+FOUNDATION_EXPORT double PokipediaFoundationVersionNumber;
+
+//! Project version string for PokipediaFoundation.
+FOUNDATION_EXPORT const unsigned char PokipediaFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokipediaFoundation/PublicHeader.h>
